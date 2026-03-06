867852ead9dfe730d383d0356fbd3c77e473c436
ryanvetoriano <vetorianosilva@gmail.com>
2026-03-06 20:32:23 -0300

    Adicionando validação em Genero.cs e arrumando erro de digitação

--- CentralDoSaber.Domain/Entities/Genero.cs
+++ CentralDoSaber.Domain/Entities/Genero.cs
@@ -5,13 +5,16 @@ namespace CentralDoSaber.Domain.Entities;
 public class Genero : BaseEntity
 {
     public string Nome { get; private set; }
-    
+
     public string Descricao { get; private set; }
-    
-    public IReadOnlyCollection<Conteudo> Contents { get; private set; }
-    
-    public Genero(string nome,  string descricao)
+
+    public IReadOnlyCollection<Conteudo> Conteudos { get; private set; } = new List<Conteudo>();
+
+    public Genero(string nome, string descricao)
     {
+        if (string.IsNullOrWhiteSpace(nome))
+            throw new Exception("Nome do gênero não pode ser vazio.");
+
         Nome = nome;
         Descricao = descricao;
     }
