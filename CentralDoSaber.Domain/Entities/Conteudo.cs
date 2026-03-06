61f5563f3403c7b85a9f5a045742cf9c1eb322de
ryanvetoriano <vetorianosilva@gmail.com>
2026-03-06 20:24:45 -0300

    Finalizando Conteudo.cs, adicionando validações, construtor e metodos para atualizar titulo e descrição

--- CentralDoSaber.Domain/Entities/Conteudo.cs
+++ CentralDoSaber.Domain/Entities/Conteudo.cs
@@ -25,25 +25,68 @@ public class Conteudo : BaseEntity
     {
         AtualizarTitulo(titulo);
         AtualizarDescricao(descricao);
-        
+    
         if (dataLancamento < 1888 || dataLancamento > DateTime.Now.Year + 5)
             throw new Exception("Ano de lançamento inválido.");
+        
         DataLancamento = dataLancamento;
+    
+        if (generos == null || !generos.Any())
+            throw new Exception("O conteúdo deve ter ao menos um gênero.");
         
+        Generos = generos;
+
         Tipo = tipo;
-        
-       ValidarEspecificacoes(NumeroPaginas, NumeroCapitulos);
+    
+        ValidarEspecificacoes(numeroPaginas, numeroCapitulos);
     }
 
-    private void Especificacoes(int? numeroCapitulo, int? numeroCapitulos)
+    private void ValidarEspecificacoes(int? numeroPaginas, int? numeroCapitulos)
     {
         switch (Tipo)
         {
             case MediaType.Livro:
-            {
-                if 
-            }
+            case MediaType.Manga:
+            case MediaType.HQ:
+            case MediaType.Revista:
+                ValidarPaginasECapitulos(numeroPaginas, numeroCapitulos, Tipo.ToString());
+                break;
+
+            case MediaType.Outros:
+                if (numeroPaginas <= 0)
+                    throw new Exception("Conteúdos devem ter uma quantidade de páginas maior que zero.");
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException();
         }
+
+        NumeroPaginas = numeroPaginas;
+        NumeroCapitulos = numeroCapitulos;
+    }
+
+    private void ValidarPaginasECapitulos(int? numeroPaginas, int? numeroCapitulos, string tipo)
+    {
+        if (numeroPaginas <= 0)
+            throw new Exception($"{tipo}s devem ter uma quantidade de páginas maior que zero.");
+
+        if (numeroCapitulos <= 0)
+            throw new Exception($"{tipo}s devem ter ao menos um capítulo.");
+    }
+    
+    public void AtualizarTitulo(string titulo)
+    {
+        if (string.IsNullOrWhiteSpace(titulo))
+            throw new Exception("O título não pode ser vazio.");
+
+        Titulo = titulo.Trim();
+    }
+    
+    public void AtualizarDescricao(string descricao)
+    {
+        if (string.IsNullOrWhiteSpace(descricao) || descricao.Length < 10)
+            throw new Exception("A descrição deve ter pelo menos 10 caracteres.");
+        Descricao = descricao;
     }
     
 }
